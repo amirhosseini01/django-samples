18ba50d8feee1ad291fc7d172e50621ab1ccf508
amir <amircsharp7@gmail.com>
2022-12-29 15:45:32 +0330

    querying in data and create related templates

--- hello-world/mainproj/firstapp/views.py
+++ hello-world/mainproj/firstapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import FirstApp, Tag, Review
+
+def projects(request):
+    projects = FirstApp.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    project = FirstApp.objects.get(id = pk)
+    mytags = Tag.objects.all()
+    context = {'project': project, 'mytags': mytags}
+    return render(request, 'projects/project.html', context)
--- hello-world/mainproj/mainproj/settings.py
+++ hello-world/mainproj/mainproj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os # also used for template
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,7 +58,10 @@ ROOT_URLCONF = 'mainproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # also used for template
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
