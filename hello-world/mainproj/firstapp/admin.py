0fa61addaf963579db309650081a77c4cd53087a
amir <amircsharp7@gmail.com>
2022-12-29 13:27:15 +0330

    add Tag, Review Models

--- hello-world/mainproj/firstapp/admin.py
+++ hello-world/mainproj/firstapp/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import FirstApp
+from .models import FirstApp, Review, Tag
 admin.site.register(FirstApp)
+admin.site.register(Review)
+admin.site.register(Tag)
--- hello-world/mainproj/firstapp/models.py
+++ hello-world/mainproj/firstapp/models.py
@@ -6,8 +6,32 @@ class FirstApp(models.Model):
     title = models.CharField(max_length=200)
     desc = models.TextField(max_length=2000, null=True, blank=True)
     link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     createAt = models.DateField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable= False)
 
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    firstApp = models.ForeignKey(FirstApp, on_delete= models.CASCADE)
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote')
+    )
+    value  = models.CharField(max_length=200, choices=VOTE_TYPE)
+    createAt = models.DateField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable= False)
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    title = models.CharField(max_length=200)
+    createAt = models.DateField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable= False)
+    
     def __str__(self):
         return self.title
