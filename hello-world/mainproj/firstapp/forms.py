12dc3d4b84add243798708443cc05dc6bc0fdabd
amir <amir.1derful@gmail.com>
2023-01-05 12:13:10 +0330

    use custom input class + using different shape of input

--- hello-world/mainproj/firstapp/forms.py
+++ hello-world/mainproj/firstapp/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
+from django import forms
 from .models import FirstApp
 
 class ProjectForm(ModelForm):
@@ -6,3 +7,13 @@ class ProjectForm(ModelForm):
         model = FirstApp
         # fields = '__all__'
         fields = ['title', 'tags', 'img'] 
+        widgets = {
+            'tags' : forms.CheckboxSelectMultiple(),
+        }
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'my-custom-class'})
+
+        # self.fields['title'].widget.attrs.update({'class': 'my-custom-class', 'placeholder': 'add title'})
