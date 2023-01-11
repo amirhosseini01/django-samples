2605f60672d1c8f6fcfae15643ecddbffdd1194b
amir <amir.1derful@gmail.com>
2023-01-11 22:14:51 +0330

    using signals in django

--- hello-world/mainproj/users/models.py
+++ hello-world/mainproj/users/models.py
@@ -5,6 +5,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+# first option
+from django.db.models.signals import post_save, post_delete
+# second option (alternative)
+from django.dispatch import receiver
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
@@ -15,3 +20,21 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+@receiver(post_save, sender=User)
+def profileUpdate(sender, instance, created, **kwarg):
+    print('Profile Saved!')
+    if created:
+        profile = Profile.objects.create(
+            user = instance,
+            name = instance.username
+        )
+
+@receiver(post_delete, sender=User)
+def profileDelete(sender, instance, **kwarg):
+    print('Deleting Profile!')
+
+# first option
+#post_save.connect(profileUpdate, sender=Profile)
+#post_delete.connect(profileDelete, sender=Profile)
+# second option is: using "@receiver" decorator ...
