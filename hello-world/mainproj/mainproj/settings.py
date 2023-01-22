1cddc7b0641d66976554db2dfaaf4d3329b02044
amir <amir.1derful@gmail.com>
2023-01-22 23:41:54 +0330

    connect to psql db in django

--- hello-world/mainproj/mainproj/settings.py
+++ hello-world/mainproj/mainproj/settings.py
@@ -85,12 +85,24 @@ WSGI_APPLICATION = 'mainproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mysecretpassword',
+        'HOST': '172.17.0.2',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
